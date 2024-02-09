fbe5438cd0469d7ce010e34419f6d3797066f529
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-02-09 20:42:06 +0300

    Update DBConnection.java

--- src/MysqlDB/DBConnection.java
+++ src/MysqlDB/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/clearance_management_system";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Itismemysql@54";
+    private static final String PASSWORD = "root123";
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Register JDBC driver
